71c4e6b6835fa159f6d9e8fe7e42bbfe05d28f47
elizzzabethrae <elizabethwarners@gmail.com>
2018-12-03 13:57:15 -0500

    added controllers

---
+++ controllers/MenuController.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+
+module.exports = class MenuController {
+  constructor() {}
+
+main(){
+  console.log('hello from main');
+}
+
+clear(){}
+ console.log('hello from clear');
+}
